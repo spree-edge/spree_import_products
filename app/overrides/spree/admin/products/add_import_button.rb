53f7de2b0eb003407f6c5389f8d13e6179a4f5b4
Vishal <vishal@bluebash.co>
2023-08-03 16:14:22 +0530

    remove defave dependency and upgrade it with spree 4.6

--- app/overrides/spree/admin/products/add_import_button.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/admin/products/index',
-  name: 'add_import_button',
-  insert_before: "erb[loud]:contains('admin_new_product')",
-  text: "
-    <%= render 'import_form'%>
-  ")
